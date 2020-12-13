3a50c1cee515ab17717c950c963a541ad1b2c4b2
Daniel Parnell <me@danielparnell.com>
2020-12-13 11:39:11 +0000

    :fist: MQTT commands are making it to the device :fist:

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as https from 'https';
 import axios from 'axios';
 import {AxiosInstance} from 'axios';
+import * as MQTT from 'async-mqtt';
+import {AsyncMqttClient} from 'async-mqtt';
 
 const DREAMCATCHER_ROOT = "dc11.iotdreamcatcher.net";
 const APP_VERSION = "1.8.2";
@@ -19,7 +21,7 @@ interface Uib {
     token: string;
 }
 
-interface DeviceInfo {
+export interface DeviceInfo {
     auth: string;
     deviceID: string;
     productID: string;
@@ -36,18 +38,65 @@ interface DeviceInfo {
 }
 
 
+export class DeviceConnection {
+    private mqtt: AsyncMqttClient;
+    private device: DeviceInfo;
+    private alias: string;
+    private clientId: string;
+
+    constructor(mqtt: AsyncMqttClient, deviceInfo: DeviceInfo, alias: string) {
+        this.mqtt = mqtt;
+        this.device = deviceInfo;
+        this.alias = alias;
+
+        this.clientId = "android_" + Math.floor(Math.random() * 1000000);
+
+        this.mqtt.on("message", (topic, message) => {
+            const msg = JSON.parse(message.toString());
+            console.info(topic);
+            console.dir(msg, { depth: 10 });
+        });
+    }
+
+    send(message: any) {
+        this.mqtt.publish("00s/01/x/300/" + this.device.deviceID + "/post/111", JSON.stringify(message));
+    }
+
+    buildK1ActionMessage(subject: string, request: any): any {
+        return {
+            message: {
+                type: "broadcast",
+                to: this.device.deviceID,
+                from: this.clientId,
+                username: this.alias,
+                ack_mark: "0",
+                subject: subject,
+                request: request
+            }
+        };
+    }
+
+    public getAllDevices() {
+        let msg = this.buildK1ActionMessage("zwave", { action: "get_all_devices", status: "ok"});
+        this.send(msg);
+    }
+
+}
+
 export class Client {
     private client: AxiosInstance;
     private options: any;
     private username: string;
+    private alias: string;
     private uuid: string;
     private uib: Uib;
     private token: string;
 
-    private constructor(client: AxiosInstance, options: any, username: string, uuid: string, uib: Uib, token: string) {
+    private constructor(client: AxiosInstance, options: any, username: string, alias: string, uuid: string, uib: Uib, token: string) {
         this.client = axios;
         this.options = options;
         this.username = username;
+        this.alias = alias;
         this.uuid = uuid;
         this.uib = uib;
         this.token = token;
@@ -68,7 +117,12 @@ export class Client {
             }, options));
 
             if(resp.data && resp.data.status == "200") {
-                return new Client(client, options, username, uuid, uib, resp.data.token);
+                const token = resp.data.token;
+
+                resp = await client.get("/uac/GET/getUserInfo/00s/01/com.dreamcatcher.smanos/" + username + "//" + uib.token + "/" + Date.now(), options);
+                if(resp.data && resp.data.status == "200") {
+                    return new Client(client, options, username, resp.data.user.userAlias, uuid, uib, token);
+                }
             }
         }
 
@@ -83,6 +137,13 @@ export class Client {
         }
 
         throw resp.data;
+    }
+
+    public async connect(device: DeviceInfo): Promise<DeviceConnection> {
+        const client = await MQTT.connectAsync("tls://" + device.cmdIP + ":8883", { clientId: String(Date.now()), username: "and_" + device.deviceID, password: this.token, rejectUnauthorized: false});
+        client.subscribe("00s/01/x/300/" + device.deviceID +  "/set/#");
+
+        return new DeviceConnection(client, device, this.alias);
     }
 
 }
